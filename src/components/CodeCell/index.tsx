4e7f1f1044ad47571246697b43e963ff36108073
daniel-ishitani <ishitanidim@gmail.com>
2022-02-04 19:09:23 -0300

    Improved error handling

--- src/components/CodeCell/index.tsx
+++ src/components/CodeCell/index.tsx
@@ -8,12 +8,14 @@ import Resizable from '../Resizable';
 
 const CodeCell = () => {
   const [code, setCode] = useState('');
+  const [err, setErr] = useState('');
   const [input, setInput] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       const output = await bundle(input);
-      setCode(output);
+      setCode(output.code);
+      setErr(output.err);
     }, 500);
 
     return () => clearTimeout(timer);
@@ -25,7 +27,7 @@ const CodeCell = () => {
         <Resizable direction="horizontal">
           <CodeEditor initialValue='' onChange={(value) => setInput(value)}/>
         </Resizable>
-        <Preview code={code} />
+        <Preview code={code} err={err} />
       </section>
     </Resizable>
   );
--- src/components/Preview/index.tsx
+++ src/components/Preview/index.tsx
@@ -4,6 +4,7 @@ import './styles.css';
 
 interface PreviewProps {
   code: string,
+  err: string,
 };
 
 const html = `
@@ -14,13 +15,22 @@ const html = `
     <body>
       <div id="root"></div>
       <script>
+        const handleError = (err) => {
+          const root = document.querySelector('#root');
+          root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+          console.error(err);
+        };
+
+        window.addEventListener('error', (event) => {
+          event.preventDefault();
+          handleError(event.error);
+        });
+
         window.addEventListener('message', (event) => {
           try {
             eval(event.data);
           } catch (err) {
-            const root = document.querySelector('#root');
-            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-            console.error(err);
+            handleError(err);
           }
         }, false);
       </script>
@@ -29,7 +39,7 @@ const html = `
 `;
 
 
-const Preview: FC<PreviewProps> = ({ code }) => {
+const Preview: FC<PreviewProps> = ({ code, err }) => {
   const iframeRef = useRef<any>();
 
   useEffect(() => {
@@ -46,6 +56,7 @@ const Preview: FC<PreviewProps> = ({ code }) => {
         sandbox="allow-scripts"
         srcDoc={html}
       />
+      {err && <div className="preview-error">{err}</div>}
     </div>
   );
 }
